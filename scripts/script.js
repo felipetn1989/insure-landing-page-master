c2051774b58064954a92fb9c625b0998a511ad1f
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-31 16:16:52 -0300

    finished version 1

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ const headerMenu = document.querySelector(".header_menu");
 
 const h2Border = document.querySelector(".h2_border");
 
+/* Everytime the user clicks on the menu icon, it toggles appropriate classes on the header menu and the menu icon, to properly display them on mobile devices */
+
 menuIcon.addEventListener("click", () => {
   headerMenu.classList.toggle("header_menu_display");
   h2Border.classList.toggle("hide");
@@ -12,6 +14,8 @@ menuIcon.addEventListener("click", () => {
     menuIcon.src = "images/icon-hamburger.svg";
   }
 });
+
+/* This resize event listener was added so that the mobile menu doesn't stay open in case some resizes the screen without closing it, which was causing design problems */
 
 window.addEventListener("resize", () => {
   headerMenu.classList.remove("header_menu_display");
