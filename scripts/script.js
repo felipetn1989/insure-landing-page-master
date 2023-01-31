6b7f08ca9dd49e759a0c4d7f7acd06b39efc24d1
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-31 15:51:52 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,19 @@
-const headerMenu = document.querySelector('.header_menu')
+const headerMenu = document.querySelector(".header_menu");
 
-const h2Border = document.querySelector('.h2_border')
+const h2Border = document.querySelector(".h2_border");
 
 menuIcon.addEventListener("click", () => {
-    headerMenu.classList.toggle('header_menu_display')
-    h2Border.classList.toggle('hide')
+  headerMenu.classList.toggle("header_menu_display");
+  h2Border.classList.toggle("hide");
 
-    if (headerMenu.classList.contains('header_menu_display')) {
-        menuIcon.src = "images/icon-close.svg"
-    } else {
-        menuIcon.src = "images/icon-hamburger.svg"
-    }
-})
+  if (headerMenu.classList.contains("header_menu_display")) {
+    menuIcon.src = "images/icon-close.svg";
+  } else {
+    menuIcon.src = "images/icon-hamburger.svg";
+  }
+});
+
+window.addEventListener("resize", () => {
+  headerMenu.classList.remove("header_menu_display");
+  menuIcon.src = "images/icon-hamburger.svg";
+});
