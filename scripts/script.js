6e5ba8f188b45b3b5b41162d2d9545fd5b9355fe
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-31 15:46:41 -0300

    desktop menu

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const headerMenu = document.querySelector('.header_menu')
+
+const h2Border = document.querySelector('.h2_border')
+
+menuIcon.addEventListener("click", () => {
+    headerMenu.classList.toggle('header_menu_display')
+    h2Border.classList.toggle('hide')
+
+    if (headerMenu.classList.contains('header_menu_display')) {
+        menuIcon.src = "images/icon-close.svg"
+    } else {
+        menuIcon.src = "images/icon-hamburger.svg"
+    }
+})
